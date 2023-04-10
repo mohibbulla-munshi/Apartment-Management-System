3c8c31372459016a8668da59462c2425888881ef
mohib04 <mohib.agu@gmail.com>
2023-04-10 15:51:41 +0600

    logo added

--- resources/views/user_dashboard/layout/left_panel.blade.php
+++ resources/views/user_dashboard/layout/left_panel.blade.php
@@ -1,11 +1,6 @@
 <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
     <div class="sidebar-brand d-none d-md-flex">
-        <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-            <use xlink:href="{{ asset('themes/assets/brand/coreui.svg#full') }}"></use>
-        </svg>
-        <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
-            <use xlink:href="{{ asset('themes/assets/brand/coreui.svg#signet') }}"></use>
-        </svg>
+        <img style="width:50%" src="{{ asset('themes/assets/img/apartment-management-system-removebg-preview.png') }}" alt="">
     </div>
     <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
         <li class="nav-item"><a class="nav-link" href="index.html">
