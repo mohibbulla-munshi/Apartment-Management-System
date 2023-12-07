d5b1d2d9f0c33c4068630c189ac5e60c13bb16cf
mohib04 <mohib.agu@gmail.com>
2023-12-07 18:43:30 +0600

    try to add vie button

--- resources/views/user_dashboard/layout/top.blade.php
+++ resources/views/user_dashboard/layout/top.blade.php
@@ -61,3 +61,8 @@
     // Bootstrap ID
     gtag('config', 'UA-118965718-5');
 </script>
+
+<!-- Include jQuery and Bootstrap scripts at the beginning of your HTML file -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
